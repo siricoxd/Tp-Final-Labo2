be6d32a877760ea2ee69267c68936cbcbc68184d
siricoxd <martindelollio@gmail.com>
2023-10-31 16:23:00 -0300

    test

---
+++ tp final/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    int i=0;
+    while(i<10)
+    {
+        printf("Hello world!\n");
+        i++;
+    }
+    return 0;
+}
