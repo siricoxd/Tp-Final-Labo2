198fb570c5b83e404e1ecd458f76889cfe3e198f
santiago9110 <149526796+santiago9110@users.noreply.github.com>
2023-11-21 21:20:54 -0300

    actualizacion depos,deposSucu,ventas

---
+++ deposito y catalogoSucursales2/menus.h
@@ -0,0 +1,25 @@
+#ifndef MENUS_H_INCLUDED
+#define MENUS_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include "listasProductos.h"
+
+#define ARCHIVO_SUCUSALDep "depsucursal.bin"
+#include <windows.h>
+#include "depositoSucursal.h"
+#include "ventas.h"
+
+void imprimirBarraCarga(int porcentaje);
+void barra();
+
+int menuDeposito();
+int menuOpcionDeposito();
+
+int menuOpcionDepositoSucursal();
+int menuDepositoSucursales();
+
+
+int menuOpcionVentas();
+int menuVentas();
+int mainMenu();
+#endif // MENUS_H_INCLUDED
